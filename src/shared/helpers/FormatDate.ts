288d2c0d94063ed1207f5dd98e2bb421d7b23a32
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-10-21 09:48:03 -0600

    fix(helpers): :hammer: paremeter config

--- src/shared/helpers/FormatDate.ts
+++ src/shared/helpers/FormatDate.ts
@@ -1,25 +1,26 @@
 export function formatoPersonalizado(fecha: Date): string {
-  const ahora = new Date();
-  const diferencia = Math.floor((ahora.getTime() - fecha.getTime()) / 1000); // Diferencia en segundos
-
-  if (diferencia < 60) {
-      return `Hace ${diferencia} segundos`;
-  } else if (diferencia < 3600) {
-      const minutos = Math.floor(diferencia / 60);
-      return `Hace ${minutos} minutos`;
-  } else if (diferencia < 86400) {
-      const horas = Math.floor(diferencia / 3600);
-      return `Hace ${horas} horas`;
-  } else if (
-      fecha.getDate() === ahora.getDate() - 1
-  ) {
-      return `Ayer`;
-  } else if (
-      fecha.getDate() === ahora.getDate() - 2
-  ) {
-      return 'Antier';
-  } else {
-      const opcionesDeFecha = { year: 'numeric', month: 'long', day: 'numeric' };
-      return fecha.toLocaleDateString('es-ES', opcionesDeFecha);
-  }
+    const ahora = new Date();
+    const diferencia = Math.floor((ahora.getTime() - fecha.getTime()) / 1000); // Diferencia en segundos
+  
+    if (diferencia < 60) {
+        return `Hace ${diferencia} segundos`;
+    } else if (diferencia < 3600) {
+        const minutos = Math.floor(diferencia / 60);
+        return `Hace ${minutos} minutos`;
+    } else if (diferencia < 86400) {
+        const horas = Math.floor(diferencia / 3600);
+        return `Hace ${horas} horas`;
+    } else if (
+        fecha.getDate() === ahora.getDate() - 1
+    ) {
+        return `Ayer`;
+    } else if (
+        fecha.getDate() === ahora.getDate() - 2
+    ) {
+        return 'Antier';
+    } else {
+        // Usamos un formato de fecha como cadena directamente
+        return fecha.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' });
+    }
 }
+  
