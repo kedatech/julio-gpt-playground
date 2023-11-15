0125de7f97a1be4bd9b6837f350f4b6504e991ca
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-11-15 13:18:23 -0600

    feat: :sparkles: update message components

--- src/app/chat/components/BotMessage.tsx
+++ src/app/chat/components/BotMessage.tsx
@@ -2,24 +2,26 @@ import React from 'react';
 import Image from 'next/image'
 import { formatoPersonalizado } from '@/shared/helpers/FormatDate';
 
-export const BotMessage = ({ message }: { message: string }) => {
+export const BotMessage = ({ message, datetime }: { message: string, datetime: string}) => {
 
-  const fechaFormateada = formatoPersonalizado(new Date('2023-10-21 07:35:00'));
+  const fechaFormateada = formatoPersonalizado(new Date(datetime));
 
   return (
     <div className="flex w-full mt-2 space-x-3 max-w-xs">
       <div className='relative h-full w-8 flex-shrink-0'>
 
-        <div className="absolute bottom-0 h-8 w-8 rounded-full bg-gray-100 overflow-hidden">
+        <div className="absolute h-8 w-8 rounded-full bg-gray-100 overflow-hidden">
           <Image width={60} height={60} src="/julio-perfil-image.svg" alt="julio" />
         </div>
       </div>
+
       <div>
-        <div className="bg-gray-100 text-gray-600 p-3 rounded-r-2xl rounded-tl-2xl">
+        <div className="bg-gray-100 text-gray-600 p-3 rounded-r-2xl rounded-bl-2xl">
           <p className="text-sm font-medium">{message}</p>
         </div>
         <span className="text-xs text-gray-500 leading-none">{fechaFormateada}</span>
       </div>
+
     </div>
   );
 };
--- src/app/chat/components/UserMessage.tsx
+++ src/app/chat/components/UserMessage.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { formatoPersonalizado } from '@/shared/helpers/FormatDate';
 
 
-export const UserMessage = ({ message }: { message: string}) => {
-  const fechaFormateada = formatoPersonalizado(new Date());
+export const UserMessage = ({ message, datetime }: { message: string, datetime: string}) => {
+  const fechaFormateada = formatoPersonalizado(new Date(datetime));
 
   return (
     <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-      <div>
-        <div className="bg-[#2F80ED] text-white p-3 rounded-l-2xl rounded-br-2xl">
+      <div className='flex flex-col'>
+        <div className="bg-[#2F80ED] text-white p-3 rounded-l-2xl rounded-br-2xl overflow-x-hidden">
           <p className="text-sm">{message}</p>
         </div>
-        <span className="text-xs text-gray-500 leading-none">{fechaFormateada}</span>
+        <span className="text-xs text-gray-500 max-w-xs my-2 leading-none text-right">{fechaFormateada}</span>
       </div>
       {/* <div className="flex-shrink-0 text-[18px] font-bold text-gray-700 grid place-content-center h-10 w-10 rounded-full bg-gray-300">Tu</div> */}
     </div>
