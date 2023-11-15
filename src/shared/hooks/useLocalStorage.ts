894609b983af4baa20a7fa92f86ff36fbf95c983
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-11-15 13:17:49 -0600

    feat: :sparkles: add useLocalStorage

---
+++ src/shared/hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  // Obtiene el valor actual del localStorage o utiliza el valor inicial proporcionado
+  const storedValue = localStorage.getItem(key);
+  const initial = storedValue ? JSON.parse(storedValue) : initialValue;
+
+  // Crea un estado local para el valor
+  const [value, setValue] = useState<T>(initial);
+
+  // Almacena el valor en localStorage cuando cambia
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
