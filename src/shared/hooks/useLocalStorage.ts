4f26a59ae09015c886a5de63d38b47aa4a2855ff
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-11-16 10:11:14 -0600

    fix: :bug: localstorage

--- src/shared/hooks/useLocalStorage.ts
+++ src/shared/hooks/useLocalStorage.ts
@@ -2,17 +2,22 @@
 import { useState, useEffect } from 'react';
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
+  // Verifica si el código se está ejecutando en el lado del cliente antes de usar localStorage
+  const isClient = typeof window !== 'undefined';
+
   // Obtiene el valor actual del localStorage o utiliza el valor inicial proporcionado
-  const storedValue = localStorage.getItem(key);
+  const storedValue = isClient ? localStorage.getItem(key) : null;
   const initial = storedValue ? JSON.parse(storedValue) : initialValue;
 
   // Crea un estado local para el valor
   const [value, setValue] = useState<T>(initial);
 
-  // Almacena el valor en localStorage cuando cambia
+  // Almacena el valor en localStorage cuando cambia (solo si se está ejecutando en el lado del cliente)
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    if (isClient) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value, isClient]);
 
   return [value, setValue] as const;
 }
